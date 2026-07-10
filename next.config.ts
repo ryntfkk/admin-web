96cc750e31841f586615e3d9225b316c07ad5bdd
Ryan taufik <86566731+ryntfkk@users.noreply.github.com>
2026-07-11 01:21:02 +0700

    new

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  allowedDevOrigins: ['192.168.0.127'],
-  /* config options here */
+  output: 'standalone',
 };
 
 export default nextConfig;
